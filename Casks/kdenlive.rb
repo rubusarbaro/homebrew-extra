20be06c80e04931fe1c1038f6bf3d36f600577e5
Saŭlo Rubusarbaro <61021296+rubusarbaro@users.noreply.github.com>
2021-12-24 01:43:41 -0600

    Update in cask
    Changed url

--- Casks/kdenlive.rb
+++ Casks/kdenlive.rb
@@ -1,8 +1,9 @@
 cask "kdenlive" do
-  version "21.12.0-581"
+  version "21.12.0"
+  release "581"
   sha256 "39db55371eaa4794236b54d501bb9ad49f93bbc4b4434371e223159a7d321aef"
   
-  url "https://build-artifacts.kde.org/binary-factory/Kdenlive_Stable_macos/lastSuccessfulBuild/kdenlive-#{version}-macos-64-clang.dmg",
+  url "https://binary-factory.kde.org/job/Kdenlive_Stable_macos/#{release}/artifact/kdenlive-#{version}-#{release}-macos-64-clang.dmg",
       verified: "https://github.com/KDE/kdenlive"
   name "Kdenlive"
   desc "Open-source proffesional video editor"
