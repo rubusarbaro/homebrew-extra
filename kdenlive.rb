de36a1c2000ca4ead1588394fb743842c9690e6b
Saŭlo Rubusarbaro <61021296+rubusarbaro@users.noreply.github.com>
2021-12-24 01:08:08 -0600

    Create kdenlive.rb

---
+++ kdenlive.rb
@@ -0,0 +1,4 @@
+cask "kdenlive" do
+  version "971"
+  
+  url "https://build-artifacts.kde.org/binary-factory/Kdenlive_Nightly_macos/lastSuccessfulBuild/kdenlive-master-#{version}-macos-64-clang.dmg"
