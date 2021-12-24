016b97a2a20ee0ab7e2043c25a1610430e473407
Saŭlo Rubusarbaro <61021296+rubusarbaro@users.noreply.github.com>
2021-12-24 01:30:17 -0600

    Create kdenlive cask

---
+++ Casks/kdenlive.rb
@@ -0,0 +1,19 @@
+cask "kdenlive" do
+  version "21.12.0-580"
+  sha256 "39db55371eaa4794236b54d501bb9ad49f93bbc4b4434371e223159a7d321aef"
+  
+  url "https://build-artifacts.kde.org/binary-factory/Kdenlive_Stable_macos/lastSuccessfulBuild/kdenlive-#{version}-macos-64-clang.dmg",
+      verified: "https://github.com/KDE/kdenlive"
+  name "Kdenlive"
+  desc "Open-source proffesional video editor"
+  homepage "https://kdenlive.org/"
+
+  app "kdenlive.app"
+  
+  zap trash: [
+  "~/Library/Application Support/kdenlive",
+  "~/Library/Caches/kdenlive",
+  "~/Library/Preferences/kdenlive-layoutsrc",
+  "~/Library/Preferences/kdenliverc",
+]
+end
