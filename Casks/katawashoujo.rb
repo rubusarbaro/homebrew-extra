c36037dfe47cf2a6cfc23f2204eda9ec37739f30
Saŭlo Rubusarbaro <61021296+rubusarbaro@users.noreply.github.com>
2022-02-04 11:57:14 -0600

    Update katawashoujo.rb

--- Casks/katawashoujo.rb
+++ Casks/katawashoujo.rb
@@ -2,7 +2,7 @@ cask "katawashoujo" do
   version "1.3.1"
   sha256 "49ce41025a49b1f7e370dad9f3e554de7ec6d740e8a832909ff95e1c35b7d98f"
   
-  url 'http://dl.katawa-shoujo.com/gold_1.3.1/[4ls]_katawa_shoujo_#{version}-[mac][1DFC84A6].dmg'
+  url "http://dl.katawa-shoujo.com/gold_1.3.1/[4ls]_katawa_shoujo_#{version}-[mac][1DFC84A6].dmg"
     verified "katawa-shoujo.com"
   name "Katawa Shoujo"
   desc "Bishoujo-style visual game"
