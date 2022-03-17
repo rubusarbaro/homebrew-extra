746327df39482e06c2723c6baf0f9d255c9c2f54
Saŭlo Rubusarbaro <61021296+rubusarbaro@users.noreply.github.com>
2022-03-16 20:19:19 -0600

    Update to stable version

--- Casks/kdenlive.rb
+++ Casks/kdenlive.rb
@@ -1,9 +1,9 @@
 cask "kdenlive" do
-  version "21.12.0"
-  release "581"
-  sha256 "6b0af6ba2450de1fdca0be09ede451917a67ce54bc4d5d5ead44077398fa2d9c"
+  version "21.12.3"
+  release "633"
+  sha256 "d4bb0fa6b44f07e20b35bc465778f638d788be133a3b489323e5ba70aee896fa"
   
-  url "https://binary-factory.kde.org/job/Kdenlive_Stable_macos/#{release}/artifact/kdenlive-#{version}-#{release}-macos-64-clang.dmg",
+  url "https://binary-factory.kde.org/view/MacOS/job/Kdenlive_Stable_macos/lastSuccessfulBuild/artifact/kdenlive-#{version}-#{release}-macos-64-clang.dmg",
       verified: "https://github.com/KDE/kdenlive"
   name "Kdenlive"
   desc "Open-source proffesional video editor"
