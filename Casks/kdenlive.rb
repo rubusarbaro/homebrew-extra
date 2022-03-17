6125e997ede605b5696c338d84dd6e7a97c70ed2
Saŭlo Rubusarbaro <61021296+rubusarbaro@users.noreply.github.com>
2022-03-16 20:31:31 -0600

    Update Kdenlive cask
    Error in url

--- Casks/kdenlive.rb
+++ Casks/kdenlive.rb
@@ -2,7 +2,8 @@ cask "kdenlive" do
   version "21.12.3-633"
   sha256 "d4bb0fa6b44f07e20b35bc465778f638d788be133a3b489323e5ba70aee896fa"
   
-  url "https://binary-factory.kde.org/view/MacOS/job/Kdenlive_Stable_macos/lastSuccessfulBuild/artifact/kdenlive-#{version}-macos-64-clang.dmg",
+  
+  url "https://build-artifacts.kde.org/binary-factory/Kdenlive_Stable_macos/lastSuccessfulBuild/kdenlive-#{version}-macos-64-clang.dmg",
       verified: "https://github.com/KDE/kdenlive"
   name "Kdenlive"
   desc "Open-source proffesional video editor"
